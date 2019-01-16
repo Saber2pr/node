eef46f825010771717396c3f19c49e423252ef87
saber2pr <saber2pr@gmail.com>
2019-01-16 23:14:48 +0000

    add remove func

--- src/test/test_saber_node.ts
+++ src/test/test_saber_node.ts
@@ -3,5 +3,6 @@ export async function test_saber_node() {
   // Terminal.Print.error('test')
   // Terminal.Print.success('test')
   // Terminal.Print.tips('test')
+  File.remove('./_test')
   return
 }
