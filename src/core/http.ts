9ba337c8281197ee7e9fc941482bfe94278b6ea5
saber2pr <saber2pr@gmail.com>
2019-05-01 17:30:31 +0000

    add http exception

--- src/core/http.ts
+++ src/core/http.ts
@@ -2,10 +2,11 @@
  * @Author: saber2pr
  * @Date: 2019-04-30 20:05:30
  * @Last Modified by: saber2pr
- * @Last Modified time: 2019-05-01 14:57:45
+ * @Last Modified time: 2019-05-01 17:28:04
  */
 import { Header } from './http/header'
 import { query as Query } from './http/query'
+import { HttpException } from './http/httpException'
 
 export namespace Http {
   export const cookie = (obj: Object) =>
@@ -16,4 +17,6 @@ export namespace Http {
   export const query = Query
 
   export const header = new Header()
+
+  export const Exception = HttpException
 }
---
+++ src/core/http/httpException.ts
@@ -0,0 +1,25 @@
+/*
+ * @Author: saber2pr
+ * @Date: 2019-05-01 17:23:32
+ * @Last Modified by: saber2pr
+ * @Last Modified time: 2019-05-01 17:29:18
+ */
+import { ServerResponse } from 'http'
+import { Headers } from './header'
+
+export class HttpException extends Error {
+  constructor(public message: string, public code: number) {
+    super(message)
+  }
+  static resolve(
+    HttpException: any,
+    response: ServerResponse,
+    headers?: Headers
+  ) {
+    return new Promise(resolve => {
+      const err: HttpException = HttpException
+      response.writeHead(err.code, <any>headers)
+      response.end(err.message, resolve)
+    })
+  }
+}
