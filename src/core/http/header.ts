128b9b2a644812c2251337b6f9ee84608ea56b25
saber2pr <saber2pr@gmail.com>
2019-05-06 20:09:17 +0000

    update: httpheaders

--- src/core/http/header.ts
+++ src/core/http/header.ts
@@ -9,6 +9,7 @@ export type Headers =
   | 'Access-Control-Allow-Methods'
   | 'Access-Control-Allow-Credentials'
   | 'Access-Control-Allow-Origin'
+  | 'Access-Control-Max-Age'
   | 'Accept-Patch'
   | 'Accept-Ranges'
   | 'Age'
