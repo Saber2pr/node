f35020c76684491851084da28c988c49449ca001
saber2pr <saber2pr@gmail.com>
2019-06-11 20:22:47 +0000

    revert(mkFile): please use mkPath + writeFile instead.

--- src/test/file.ts
+++ src/test/file.ts
@@ -4,7 +4,11 @@ async function test() {
   // const files = await FS.search(process.cwd() + '/src', 'file', 2)
 
   // console.log(files)
-  await FS.mkFile('./__test/ss/aa.ts', 'aa')
+  await FS.mkPath('./__test/ss/vv/')
+  await Promise.all([
+    FS.writeFile('./__test/ss/vv/aa.js', 'aa'),
+    FS.writeFile('./__test/ss/vv/bb.js', 'aa')
+  ])
 }
 
 test()
